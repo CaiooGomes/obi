19bde77a7271cbf34c36c31e7995172edf6e52c2
CaiooGomes <gomescaio898@gmail.com>
2023-05-12 15:22:17 -0300

    caixas

---
+++ Caixa.py
@@ -0,0 +1,21 @@
+caixaA = int(input())
+caixaB = int(input())
+caixaC = int(input())
+
+
+lista = [caixaA, caixaB, caixaB]
+lista.sort()
+
+menor = lista[0]
+medio = lista[1]
+maior = lista[2]
+
+AT = medio + menor
+
+quantas = 
+
+if (AT <= maior): 
+
+
+
+
---
+++ bonde.py
@@ -0,0 +1,11 @@
+professores = int(input())
+Alunos = int(input())
+quantidadeMax = 50
+
+possivel = Alunos/professores
+
+if (possivel >= quantidadeMax): print('Nao é possivel')
+else:     print('é possivel')
+
+
+
